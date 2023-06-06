cb4536f8e039703a086a155a50cc23765dfc2789
S.Watanabe <ocelot2828@gmail.com>
2023-06-06 19:52:07 +0900

    feat(sitemap.xml): add Metadata Files API

---
+++ src/app/sitemap.ts
@@ -0,0 +1,23 @@
+import getAllPosts from 'lib/getAllPosts';
+
+async function getPosts() {
+  const posts = await getAllPosts();
+  return posts;
+}
+
+export default async function sitemap() {
+  const posts = await getPosts();
+  const url = process.env.PROD_URL;
+
+  const postss = posts.map((post) => ({
+    url: url + '/' + post.slug,
+    lastModified: new Date(post.date).toUTCString(),
+  }));
+
+  const routes = ['', '/posts', '/mail'].map((route) => ({
+    url: url + route,
+    lastModified: new Date().toUTCString(),
+  }));
+
+  return [...routes, ...postss];
+}
