905feb62b7324033ed4444f0e26b3a72f2f10708
S.Watanabe <ocelot2828@gmail.com>
2023-06-07 09:46:54 +0900

    fix(page): revert

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,4 @@
 import { Metadata } from 'next';
-import Image from 'next/image';
-import profilePic from '@public/alum.png';
 import generateSEOData from 'lib/generateSEOData';
 
 export const metadata: Metadata = generateSEOData({ title: 'Home', subtitle: 'About', date: '1994' });
@@ -8,14 +6,13 @@ export const metadata: Metadata = generateSEOData({ title: 'Home', subtitle: 'Ab
 const Page = (): JSX.Element => {
   return (
     <>
-      <Image className="main_icon" src={profilePic} alt="icon" quality={100} width={80} height={80} />
-      <p className="p__width">
-        I am a designer developer based in Tokyo. <br />I spend my time on the{' '}
-        <a href="https://works.su-pull.net/">projects</a>, I was self taught drawing and design, My incentive is to be
-        involved in the most cutting-edge the fields.
+      <p>
+        I am a person who designs and creates software in Tokyo.
+        <br />I like to work on
+        <a href="https://works.su-pull.net/"> my-projects</a> and I want to be part of the newest and most advanced
+        areas.
       </p>
-      <hr />
-      <h1>Career</h1>
+      <h1>I history</h1>
       <ul>
         <li>1994 born in Kitakyushu, Fukuoka, Japan</li>
         <li>2014 traveled to the USA, Seattle, and Canada</li>
